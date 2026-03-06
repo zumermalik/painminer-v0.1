39cf7848fe4a28b7fd23159a5fc99f2747c7f5ec
zhq0 <zumermalik48@gmail.com>
2026-03-06 20:14:02 +0500

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#3b82f6",
+          dark: "#2563eb",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
